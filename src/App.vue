4ff9d74906ac4eacd04b4b82c413d68b15b5e939
Stanley-92 <leystan405@gmail.com>
2025-12-25 23:08:47 +0700

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
 <router-view></router-view>
 </template>
+
+
+<script>
+
+</script>
+
+<style>
+/* Global cursor fix */
+body, div, p, span {
+  user-select: none;        /* Can't select text */
+  caret-color: transparent; /* Hide the blinking cursor */
+}
+
+input, textarea {
+  user-select: text;
+  caret-color: auto;        /* Show cursor normally */
+}
+</style>
